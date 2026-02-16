bdf02ac6f3cb08426c022fadce707cbf28c1bce0
Carlo Segat (TUB) <carlo.segat@tu-berlin.de>
2026-02-16 10:35:26 +0100

    fixed rip grep display result

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -13,5 +13,18 @@ return {
             { "<C-p>", ":Files<CR>", desc = "FZF: find files by name" },
             { "<C-s>", ":Rg<CR>", desc = "FZF: search file contents (rg)" },
         },
+        config = function()
+            -- Ensure preview highlights match the actual search results
+            vim.g.fzf_preview_window = { 'right:50%', 'ctrl-/' }
+            -- Use exact matching for Rg to avoid highlight mismatches (disable fuzzy matching)
+            vim.api.nvim_create_user_command('Rg', function(opts)
+                vim.fn['fzf#vim#grep'](
+                    'rg --column --line-number --no-heading --color=always --smart-case -- ' .. vim.fn.shellescape(opts.args),
+                    1,
+                    vim.fn['fzf#vim#with_preview']({ options = '--exact --delimiter : --nth 3..' }),
+                    0
+                )
+            end, { nargs = '*' })
+        end,
     },
 }
